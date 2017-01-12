d3c978d2c5a9beb41eb085f720bb37b7a245489f
bloodtear <xiaoyu1120520@126.com>
2017-01-12 15:55:47 +0800

    去掉无用的auth_token_and_go函数。直接进入对应页面

--- controller/picservice_controller.php
+++ controller/picservice_controller.php
@@ -18,58 +18,7 @@ class picservice_controller {
     public function get_pic_url_ajax() {
         return array("ret" => "success", "info" =>PICSERVICE_URL);
     }
-    
-    public function auth_token_go_action () {
-        header("Access-Control-Allow-Origin: " . PICSERVICE_IP );
-        $act = get_request("act");
-        $tourl = get_request("tourl");
-        logging::e("tourl", $tourl);
-        if ($act == 'auth') {
-            $token = picservice::get_token();
-            $token = $token['token'];
-            $tourl = urlencode($tourl);
-            $url = PICSERVICE_URL . "?picservice/auth_token". "&token=$token&tourl=$tourl";
-            logging::e("LINK TO", $url);
-            header("Location: " .$url);
-            return;
-        }
-        
-        if ($act == 'authret') {
-            $ret = get_request("ret");
-            if ($ret == 'success') {
-                logging::e("tourl", $tourl);
-                header("Location: " .$tourl);
-            } else if($ret == 'fail'){
-                logging::e("Auth token fail", "now refresh token from code & host");
-                $code = picservice::get_code();
-                $code = $code["value"];
-                $host = $_SERVER['REQUEST_SCHEME'] . "://". $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                $host = explode('?',$host);
-                $host = $host[0];
-                logging::e("fileget",PICSERVICE_URL . "/ajax.php?action=picservice.request_token&code=$code&host=$host");
-                $ret = file_get_contents(PICSERVICE_URL . "/ajax.php?action=picservice.request_token&code=$code&host=$host");
-                $ret = json_decode($ret);
-                logging::e("fileget ret :", $ret);
-                if ($ret->ret == 'failed') {
-                    echo "picservice_token 校验失败！";
-                    return;
-                }
-                $token = $ret->token;
-                $expired = $ret->expired;
-                $sa_ret = picservice::save_token($token, $expired);
-                if ($sa_ret){
-                    $tourl = urlencode($tourl);
-                    $url = "?picservice/auth_token_go&act=auth&tourl=$tourl";
-                    logging::e("LINK TO", $url);
-                    header("Location: " . $url);
-                    return;
-                }
-                echo "token 存储失败";
-                return;
-            }
-        }
-    }
-    
+   
     public function get_token_ajax() {
         $ret = picservice::get_token();
         return array("ret" => "success", "info" =>$ret);
@@ -83,13 +32,7 @@ class picservice_controller {
         return $ret ? array("ret" => "success") : array("ret" => "fail", "reason" => "save_token_fail");
     }
     
-
 }
-
-
-
-
-
 
 
 
