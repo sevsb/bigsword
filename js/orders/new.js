10846d5dc84295c2d7148c49b33e1611ad6a7b5d
bloodtear <xiaoyu1120520@126.com>
2017-02-09 12:15:44 +0800

    1、提交了order中，先点击staff在筛选service-item的效果。 2、针对选中红色的内容未做处理。按照之前讨论的是选中红色的，红色左移，并且清零中间内容。

--- js/orders/new.js
+++ js/orders/new.js
@@ -5,10 +5,6 @@ $(document).ready(function() {
         if ($(this).hasClass('cannot_choose')) {
             return;
         }
-        if ($(this).hasClass("selected")) {
-            $(this).parents('.order_item_body').find('.item_show').removeClass('selected');
-            return;
-        }
         $(this).parents('.order_item_body').find('.item_show').removeClass('selected');
         $(this).addClass("selected"); 
     });
@@ -20,7 +16,6 @@ $(document).ready(function() {
     $('.service_item_div').find('.item_show').click( function() {
         item_id = $(this).find('.item_id').html();
         console.log(item_id);
-        
         var ability_group = [];
         for (staff_id in staff_services) {
             var skills = staff_services[staff_id].service_id;
@@ -33,7 +28,7 @@ $(document).ready(function() {
                 }
             }
         }
-        console.log(ability_group);
+        console.log(ability_group); //筛选出会此技能的组。
         $('.servers_div').find('.item_show').each(function(){
             var staff_id = $(this).attr('id');
             ab_length = ability_group.length;
@@ -48,5 +43,37 @@ $(document).ready(function() {
     });
     
     
+    $('.servers_div').find('.item_show').click( function() {
+        var staff_id = $(this).attr("id");
+        console.log(staff_id);
+        var staff_skills = null;
+        for (id in staff_services) {
+            var skills = staff_services[id].service_id;
+            var stf_id = staff_services[id].staff_id;
+            skills = skills.split(",");
+            if(staff_id == stf_id) {
+                staff_skills = skills;
+            }
+        }
+        console.log(staff_skills);  //筛选出此人会的技能
+        
+        $('.service_item_div').find('.item_show').each(function(){
+            var item_id = $(this).find('.item_id').html();
+            $(this).addClass('disability');
+            if (staff_skills == null) {
+                return;
+            }
+            sk_length = staff_skills.length;
+            for (var i = 0; i < sk_length; i++) {
+                if (staff_skills[i] == item_id){
+                    $(this).removeClass('disability');
+                }
+            }
+        });
+        
+        
+        
+        
+    });
 });
 
