14526597bc8564bd925fb2f95dfc0f101591c62f
bloodtear <xiaoyu1120520@126.com>
2017-03-03 15:52:30 +0800

    解决 #2 订单列表默认是未完成在上，已完成在下 现在订单列表中已完成在下了。

--- js/orders/index.js
+++ js/orders/index.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     //check_picservice_token();
-    $('#main_table').DataTable();
+    $('#main_table').DataTable( {
+              //跟数组下标一样，第一列从0开始，这里表格初始化时，第四列默认降序
+                "order": [[ 1, "desc" ]]
+    });
+
+
     
     $('.done').click(function (){
         id = $(this).parents('tr').attr('id');
