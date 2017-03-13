2c7bec3ec72852485496e14340a18021a45728c2
bloodtear <xiaoyu1120520@126.com>
2017-03-13 12:13:09 +0800

    1 修正staff_status页面head自动换行问题 2 添加staff_status页面放大缩小功能。

--- js/orders/staff_orders.js
+++ js/orders/staff_orders.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    var clock = $('#page-wrapper').attr('clock');
+    var scale = 3;
+    var width = 18;
     
     $('.time_block').mouseover(function (){
         if($(this).hasClass('not_work')) {
@@ -21,6 +24,73 @@ $(document).ready(function() {
         }
         document.location.href = url;
     });
+    
+    $('.zoom_out').click(function (){
+        if (scale < 3) {
+            width += 6;
+            scale++;
+            zoom(width);
+            console.log(width);
+            console.log(scale);
+            time_scale_change(scale, width);
+        }
+        
+    });
+    
+    $('.zoom_in').click(function (){
+        if (scale > 1) {
+            width -= 6;
+            scale--;
+            zoom(width);
+            console.log(width);
+            console.log(scale);
+            time_scale_change(scale, width);
+        }
+    });
    
+
 });
 
+function zoom(width) {
+    var clock = $('#page-wrapper').attr('clock');
+    $('.time_block').each(function (){
+        $(this).css("width",width + "px");
+    });
+    $('.staff_status_body').each(function (){
+        min_width = clock * 6 * width;
+        $(this).css('min-width', min_width + 'px');
+    });
+}
+
+function time_scale_change(scale, width) {
+    var scale = parseInt(scale);
+    var start_clock = parseInt($('#page-wrapper').attr('start_clock'));
+    var clock =  parseInt($('#page-wrapper').attr('clock'));
+    min_width = clock * 6 * width;
+    output = '';
+    j = 0;
+    for(i = 0; i < clock * 6; i++) {
+        echo ='';
+        if(scale == 3) {
+            if(i % 6 == 0){
+                echo = start_clock + j + ":00" ;
+                j++;
+            }
+        }else {
+            if(i % 12 == 0){
+                echo = start_clock + j + ":00" ;
+                j += 2;
+            }
+        }
+        output += "<div class='time_block_info'>" + echo + "</div>";
+    }
+    $('#time_scale').html(output);
+    $("#time_sacle").css('min-width', min_width + 'px');
+    $('.time_block_info').each(function (){
+        $(this).css("width", width + 'px');
+        $(this).css("margin-left",'0px');
+    });
+
+
+    
+}
